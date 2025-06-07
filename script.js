f3d3efa1bf6d3ea9830ab8f5f6ef7d7c2aa982b8
Rahul Panda <rahulpanda7851@gmail.com>
2025-06-07 12:47:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,15 @@ const totalscore = () =>{
     // });
 }  
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
+
+shuffleArray(quiz);
 showques();
 nextbtn.addEventListener('click',()=>{
     const selectedchoice = document.querySelector('.choice.selected');
